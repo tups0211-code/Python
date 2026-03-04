e33898acc253768d94bbdc73a52dc89040174084
Trupti Gunge <gungetups0211@gmail.com>
2026-03-05 00:48:57 +0530

    Create stringMethods.py

---
+++ String/stringMethods.py
@@ -0,0 +1,23 @@
+#upper method
+str1 = "trupti"
+print(str1.upper())
+
+#count method(count repetation of char)
+print(str1.count("t"))
+
+#lower method
+print(str1.lower())
+
+#title method 
+print(str1.title())
+
+# capitalize method
+print(str1.capitalize())
+
+# find method(find the index)
+print(str1.find("p"))
+
+# replace method
+str2 = "trupti is smart"
+print(str2.replace("smart","intelligent"))
+
