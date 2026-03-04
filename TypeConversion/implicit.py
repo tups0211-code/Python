b9d411448e06d8b8889a01aa66471f5f09380de3
Trupti Gunge <gungetups0211@gmail.com>
2026-03-05 00:54:51 +0530

    Create implicit.py

---
+++ TypeConversion/implicit.py
@@ -0,0 +1,8 @@
+x = 5
+y = 10.5
+
+sum = x+y
+print(sum)
+print(type(sum))
+#so in implicit conversion python automatically cover 
+#type of the given data in this case there is a no need of manually conversion 
