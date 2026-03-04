9b30a16bdd104bf7d8e343b583d9727e307e560f
Trupti Gunge <gungetups0211@gmail.com>
2026-03-05 01:22:08 +0530

    Create Pattern7.py

---
+++ Patterns/Pattern7.py
@@ -0,0 +1,38 @@
+n = 5
+for i in range(n-1):
+  
+  for j in range(i,n):      #[0,5]=5space [1,5]=4 space [2,5]=3 space
+    print(" ",end=" ")
+ 
+  for j in range(i):    #left star [0] no star [1]=1 star *  [2]= 2star
+        print("*",end=" ")
+ 
+  for j in range(i+1):  #right star [1] 1star  [2]=1+2=3 star [3]=3+2 = 5 star
+        print("*",end=" ")
+  print(" ")
+
+ 
+for i in range(n):
+  
+  for j in range(i+1):       
+    print(" ",end=" ")
+ 
+  for j in range(i,n-1):  
+        print("*",end=" ")
+ 
+  for j in range(i,n):  
+     print("*",end=" ")
+  print(" ")
+
+#output : 
+
+  #         *  
+  #       * * *  
+  #     * * * * *  
+  #   * * * * * * *  
+  # * * * * * * * * *  
+  #   * * * * * * *  
+  #     * * * * *  
+  #       * * *  
+  #         *  
+
