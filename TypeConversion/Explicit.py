58691c4d0ce3a1f695978461a1b6bd20b27b0c7f
Trupti Gunge <gungetups0211@gmail.com>
2026-03-05 00:55:23 +0530

    Create Explicit.py

---
+++ TypeConversion/Explicit.py
@@ -0,0 +1,4 @@
+num = input("enter the numbers : ")
+convertedValue = float(num)
+print("orginal value is : ",num,"and data type is : ",type(num))
+print("coverted value is : ",convertedValue,"and data type is : ",type(convertedValue))
