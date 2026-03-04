e9e7fbfd1a91f93ea12f1e0ae08f17fdb8aaf8f8
Trupti Gunge <gungetups0211@gmail.com>
2026-03-04 19:49:06 +0530

    Create operator.py
    exploring the python operators like comparison operator, asingment operator, arithametic operator, logical operator

---
+++ Basic/operator.py
@@ -0,0 +1,40 @@
+
+# arithamatic operators
+x = 10
+y = 5
+
+print( x + y )
+print(x - y)
+print( x % y)
+print ( x * y)
+print(x / y)
+print (x ** y)
+
+
+#comparision operators
+print (x == y)
+print (x > y)
+print (x < y)
+print (x <= y)
+print(x >= y)
+print(x != y)
+
+
+#logical operators
+print(x > y and x < y) 
+ # both true = true
+
+print(x>=y or x<y)  
+#one true = true
+
+print("it is not operator",not(x>y))  
+#basically its convert true into false
+
+#assignment operator
+
+a = 20
+b = 21
+
+a = a+6  #bothe meaninig is same 
+a+=6
+print("it is assignment operator" , a+b)
