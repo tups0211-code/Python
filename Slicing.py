699546949fc464a4315366cd61351e59934f06f4
Trupti Gunge <gungetups0211@gmail.com>
2026-03-05 00:50:05 +0530

    Create Slicing.py

---
+++ Slicing.py
@@ -0,0 +1,10 @@
+str = "Trupti"
+
+print(str[0 : 5])      #Trupt
+print(str[ : 6])       #Trupti
+print(str[ : :])       #Trupti
+print(str[: : -1])     #itpurT
+print(str[ : : -2])    #ipr
+print(str[ : : 2])     #Tut
+print(str[ 2 : 5 ])    # upt  middle 3 char
+print(str[ 4: ])       # ti   last 2 char
