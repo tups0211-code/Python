3faa53d805e9c3100b8c9dfab42ddcc208f45d58
Trupti Gunge <gungetups0211@gmail.com>
2026-03-05 00:57:45 +0530

    Create Product.py

---
+++ Basic/Product.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter the 1st number : "))
+num2 = int(input("enter the 2nd number : "))
+
+print ("sum of num1 and num2 is : ", num1 + num2)
+print("diffrence between num1 and num2 is :", num1 - num2)
+print("product of num1 and num2 is : ", num1 ** num2)
