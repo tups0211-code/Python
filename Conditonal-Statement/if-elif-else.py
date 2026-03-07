effeb8e6ffb1c78ca66059af13d0a9435059e0da
Trupti Gunge <gungetups0211@gmail.com>
2026-03-08 01:01:23 +0530

    Create if-elif-else.py

---
+++ Conditonal-Statement/if-elif-else.py
@@ -0,0 +1,10 @@
+marks = int(input("enter your marks here : "))
+
+if marks >= 90 :
+    print("Grade A")
+elif marks >= 80 :
+    print("Grade B")
+else : 
+    print("Grade C ")
+
+print("code is end ")
