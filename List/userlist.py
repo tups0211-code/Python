61095767076d5534ed600f76cadb66e3f44cc9b0
Trupti Gunge <gungetups0211@gmail.com>
2026-03-08 01:04:18 +0530

    list using split()

---
+++ List/userlist.py
@@ -0,0 +1,11 @@
+
+food= input("enter your food1 here :").split()
+
+print(food)
+print(len(food))
+
+
+# split() is a string method used to convert a string 
+# into a list by breaking it into parts.
+
+# synatx = string.split(separator)
