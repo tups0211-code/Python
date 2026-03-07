8154d62853255fff736a792558b294829608616a
Trupti Gunge <gungetups0211@gmail.com>
2026-03-08 01:02:02 +0530

    Create list.py

---
+++ List/list.py
@@ -0,0 +1,16 @@
+food = ["gulabjam", "idli", "pohe", "panipuri"]
+
+print(food)
+print(len(food))     #length of the food
+print(food[0])       #indexing
+print(food[3])
+
+food[0] = "spdp"    #modifying list
+print(food)
+
+marks = [58, 67, 89, 76, 54, 33, 34,]
+print(marks[1:4])          #67,89,76
+print(marks[ : 3])         # 58,67,89
+print(marks[-3 : -1])       # 54,34   (34 becoz end index is excluded)
+
+    
