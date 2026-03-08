f50439f324237ee49da5379ccaf98728b440fc50
Trupti Gunge <gungetups0211@gmail.com>
2026-03-08 23:44:27 +0530

    Create dictmethods.py

---
+++ Dictonary/dictmethods.py
@@ -0,0 +1,19 @@
+student = {
+    "name" : "trupti",
+    "age" : 22,
+    "city" :"solapur",
+    "name" : "gunge"
+}
+
+print(student)
+
+print(student.items())
+
+print(student.keys())
+
+print(student.values())
+
+print(student.get("name"))
+
+student.update({"city" : "ppur"})
+print(student)
