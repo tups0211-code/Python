737e3d4defbed31d6db0fcff29767da28d15f593
Trupti Gunge <gungetups0211@gmail.com>
2026-03-08 23:43:53 +0530

    Create dict.py

---
+++ Dictonary/dict.py
@@ -0,0 +1,19 @@
+student = {
+    "name" : "trupti",
+    "age" : 22,
+    "city" :"solapur",
+    "name" : "gunge"
+}
+
+print(student)
+print(student["age"])
+
+student ["city"] = "ppur"
+print(student)
+
+print(type(student))
+student["subject"] = "maths"
+print(student)
+
+student.pop("subject")
+print(student)
