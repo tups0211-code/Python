3406e16f1229c16a047c17ab88fb193c66b1c55e
Trupti Gunge <gungetups0211@gmail.com>
2026-03-16 19:29:44 +0530

    Create class_obj.py

---
+++ Oops/class_obj.py
@@ -0,0 +1,16 @@
+# example of class and object 
+
+class student : 
+    name = "trupti gunge"  #attribute(var/data of class)
+
+s1 = student()
+print(s1.name)
+
+class vehicle : 
+    version = "cycle, car, tempo"
+    color = "black, red, yellowe"
+
+v2 = vehicle()
+print(v2.color, v2.version)
+
+
